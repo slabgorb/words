b48dcb7c87e2ac7ff3cde3f4a75d439b474d0854
Keith Avery <slabgorb@gmail.com>
2026-05-05 09:57:43 -0400

    feat(rummikub): plugin skeleton with stub manifest

---
+++ plugins/rummikub/plugin.js
@@ -0,0 +1,10 @@
+export default {
+  id: 'rummikub',
+  displayName: 'Rummikub',
+  players: 2,
+  clientDir: 'plugins/rummikub/client',
+
+  initialState: () => { throw new Error('not implemented'); },
+  applyAction: () => { throw new Error('not implemented'); },
+  publicView: () => { throw new Error('not implemented'); },
+};
