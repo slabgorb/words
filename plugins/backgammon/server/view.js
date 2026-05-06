fd5870d221c76716306337449e162603d20b6a86
Keith Avery <slabgorb@gmail.com>
2026-05-06 05:45:31 -0400

    feat(backgammon): publicView open-info passthrough + youAre

--- plugins/backgammon/server/view.js
+++ plugins/backgammon/server/view.js
@@ -1,3 +1,6 @@
-export function backgammonPublicView({ state }) {
-  return { ...state };
+export function backgammonPublicView({ state, viewerId }) {
+  let youAre = null;
+  if (state.sides?.a === viewerId) youAre = 'a';
+  else if (state.sides?.b === viewerId) youAre = 'b';
+  return { ...state, youAre };
 }
